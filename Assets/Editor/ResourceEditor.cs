908f27cddb88e039d066a56fa1b9487f6c1c82b3
BlindsidedGames <71115573+BlindsidedGames@users.noreply.github.com>
2025-06-23 22:51:57 +1000

    feat: show resource icon in preview

---
+++ Assets/Editor/ResourceEditor.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace TimelessEchoes.Upgrades
+{
+    [CustomEditor(typeof(Resource))]
+    public class ResourceEditor : Editor
+    {
+        private Resource item { get { return target as Resource; } }
+
+        public override Texture2D RenderStaticPreview(string assetPath, Object[] subAssets, int width, int height)
+        {
+            if (item.icon != null)
+            {
+                Type t = GetType("UnityEditor.SpriteUtility");
+                if (t != null)
+                {
+                    MethodInfo method = t.GetMethod("RenderStaticPreview", new[] { typeof(Sprite), typeof(Color), typeof(int), typeof(int) });
+                    if (method != null)
+                    {
+                        object ret = method.Invoke("RenderStaticPreview", new object[] { item.icon, Color.white, width, height });
+                        if (ret is Texture2D tex)
+                            return tex;
+                    }
+                }
+            }
+            return base.RenderStaticPreview(assetPath, subAssets, width, height);
+        }
+
+        private static Type GetType(string typeName)
+        {
+            var type = Type.GetType(typeName);
+            if (type != null)
+                return type;
+
+            var currentAssembly = Assembly.GetExecutingAssembly();
+            var referencedAssemblies = currentAssembly.GetReferencedAssemblies();
+            foreach (var assemblyName in referencedAssemblies)
+            {
+                var assembly = Assembly.Load(assemblyName);
+                if (assembly != null)
+                {
+                    type = assembly.GetType(typeName);
+                    if (type != null)
+                        return type;
+                }
+            }
+            return null;
+        }
+    }
+}
