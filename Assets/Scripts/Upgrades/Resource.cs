fb370c207c7ea0438c1dc563310a792079508b0b
BlindsidedGames <71115573+BlindsidedGames@users.noreply.github.com>
2025-06-23 21:48:31 +1000

    Display resource icons

--- Assets/Scripts/Upgrades/Resource.cs
+++ Assets/Scripts/Upgrades/Resource.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using Blindsided.Utilities;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace TimelessEchoes.Upgrades
 {
@@ -8,5 +11,14 @@ namespace TimelessEchoes.Upgrades
     public class Resource : ScriptableObject
     {
         public Sprite icon;
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (icon != null)
+            {
+                EditorGUIUtility.SetIconForObject(this, icon.texture);
+            }
+        }
+#endif
     }
 }
