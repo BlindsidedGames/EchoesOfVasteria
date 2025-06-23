57d6993232ed4dc3dc8ee711a10832e0253b3193
BlindsidedGames <71115573+BlindsidedGames@users.noreply.github.com>
2025-06-24 09:01:31 +1000

    Add selection listeners to UI reference classes

--- Assets/Scripts/References/UI/CostResourceUIReferences.cs
+++ Assets/Scripts/References/UI/CostResourceUIReferences.cs
@@ -11,5 +11,28 @@ namespace References.UI
         public TMP_Text countText;
         public Image selectionImage;
         public Button selectButton;
+
+        private static readonly List<CostResourceUIReferences> instances = new();
+
+        private void Awake()
+        {
+            instances.Add(this);
+            if (selectButton != null)
+                selectButton.onClick.AddListener(OnSelect);
+        }
+
+        private void OnDestroy()
+        {
+            instances.Remove(this);
+            if (selectButton != null)
+                selectButton.onClick.RemoveListener(OnSelect);
+        }
+
+        private void OnSelect()
+        {
+            foreach (var inst in instances)
+                if (inst != null && inst.selectionImage != null)
+                    inst.selectionImage.enabled = ReferenceEquals(inst, this);
+        }
     }
 }
