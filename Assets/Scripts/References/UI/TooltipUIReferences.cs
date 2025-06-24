dc2d37be4726f991b047ddea3ca3d5bdca64c10a
BlindsidedGames <blindsidedgames@outlook.com>
2025-06-24 10:49:44 +1000

    New references

---
+++ Assets/Scripts/References/UI/TooltipUIReferences.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+
+namespace References.UI
+{
+    public class TooltipUIReferences : MonoBehaviour
+    {
+        public TMP_Text resourceNameText;
+        public TMP_Text resourceCountText;
+    }
+}
