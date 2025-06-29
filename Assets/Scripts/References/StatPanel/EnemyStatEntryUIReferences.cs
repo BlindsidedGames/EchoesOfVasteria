000f526813e6f3420ec1d0f6799fd39324f4ce03
BlindsidedGames <71115573+BlindsidedGames@users.noreply.github.com>
2025-06-29 13:14:53 +1000

    Show encounter data after first kill

--- Assets/Scripts/References/StatPanel/EnemyStatEntryUIReferences.cs
+++ Assets/Scripts/References/StatPanel/EnemyStatEntryUIReferences.cs
@@ -8,6 +8,7 @@ namespace TimelessEchoes.References.StatPanel
     public class EnemyStatEntryUIReferences : MonoBehaviour
     {
         public Image enemyIconImage;
+        public TMP_Text enemyIDText;
         public TMP_Text enemyNameText;
         public TMP_Text hitpointsAndDamageText;
         public TMP_Text movementAndAttackRateText;
