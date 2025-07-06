1abc1c8a03097be6aa62a3f6268ea972e2947f59
BlindsidedGames <71115573+BlindsidedGames@users.noreply.github.com>
2025-07-07 08:09:21 +1000

    Restore StatUpgradeUIReferences script

---
+++ Assets/Scripts/References/UI/StatUpgradeUIReferences.cs
@@ -0,0 +1,14 @@
+using References.UI;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatUpgradeUIReferences : MonoBehaviour
+{
+    public CostResourceUIReferences costSlotPrefab;
+    public GameObject costGridLayoutParent;
+    public TMP_Text statUpgradeInfoText;
+    public Button upgradeButton;
+    public Button descriptionButton;
+    public GameObject descriptionPanel;
+}
