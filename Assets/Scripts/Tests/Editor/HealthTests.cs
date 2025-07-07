6b7c5208f667ea9560e34a7164e4b403527dc48d
BlindsidedGames <71115573+BlindsidedGames@users.noreply.github.com>
2025-07-07 12:31:35 +1000

    Enhance health bar visuals

--- Assets/Scripts/Tests/Editor/HealthTests.cs
+++ Assets/Scripts/Tests/Editor/HealthTests.cs
@@ -1,6 +1,8 @@
 using NUnit.Framework;
 using UnityEngine;
 using TimelessEchoes.Enemies;
+using Blindsided.Utilities;
+using System.Reflection;
 
 namespace TimelessEchoes.Tests
 {
@@ -35,6 +37,47 @@ namespace TimelessEchoes.Tests
         {
             health.TakeDamage(3f);
             Assert.AreEqual(7f, health.CurrentHealth);
+        }
+
+        [Test]
+        public void FillAmountClampedToMinimum()
+        {
+            var barObj = new GameObject();
+            var bar = barObj.AddComponent<SlicedFilledImage>();
+            var barField = typeof(Health).GetField("healthBar", BindingFlags.NonPublic | BindingFlags.Instance);
+            barField.SetValue(health, bar);
+            var minField = typeof(Health).GetField("minFillPercent", BindingFlags.NonPublic | BindingFlags.Instance);
+            minField.SetValue(health, 0.2f);
+
+            health.TakeDamage(10f);
+
+            Assert.AreEqual(0.2f, bar.fillAmount);
+            Object.DestroyImmediate(barObj);
+        }
+
+        [Test]
+        public void SpriteChangesWithHealthPercent()
+        {
+            var barObj = new GameObject();
+            var bar = barObj.AddComponent<SlicedFilledImage>();
+            var barField = typeof(Health).GetField("healthBar", BindingFlags.NonPublic | BindingFlags.Instance);
+            barField.SetValue(health, bar);
+
+            var sprite1 = Sprite.Create(Texture2D.whiteTexture, new Rect(0, 0, 1, 1), Vector2.zero);
+            var sprite2 = Sprite.Create(Texture2D.blackTexture, new Rect(0, 0, 1, 1), Vector2.zero);
+
+            var options = new Health.HealthBarSpriteOption[2];
+            options[0] = new Health.HealthBarSpriteOption { sprite = sprite1, minPercent = 0.25f };
+            options[1] = new Health.HealthBarSpriteOption { sprite = sprite2, minPercent = 0f };
+            var optField = typeof(Health).GetField("barSprites", BindingFlags.NonPublic | BindingFlags.Instance);
+            optField.SetValue(health, options);
+
+            health.TakeDamage(0f); // update bar
+            Assert.AreEqual(sprite1, bar.sprite);
+
+            health.TakeDamage(8f); // drop below 25%
+            Assert.AreEqual(sprite2, bar.sprite);
+            Object.DestroyImmediate(barObj);
         }
     }
 }
