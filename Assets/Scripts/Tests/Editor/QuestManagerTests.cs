ef74f2a83729885adb210d276a8dd5578f1b2933
BlindsidedGames <71115573+BlindsidedGames@users.noreply.github.com>
2025-07-07 13:27:06 +1000

    Add NPC-dependent quest start logic

---
+++ Assets/Scripts/Tests/Editor/QuestManagerTests.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using NUnit.Framework;
+using UnityEngine;
+using Blindsided;
+using Blindsided.SaveData;
+using TimelessEchoes.Quests;
+
+namespace TimelessEchoes.Tests
+{
+    public class QuestManagerTests
+    {
+        private GameObject oracleObj;
+        private Oracle oracle;
+        private GameObject managerObj;
+        private QuestManager manager;
+        private QuestData quest;
+
+        [SetUp]
+        public void SetUp()
+        {
+            oracleObj = new GameObject();
+            oracle = oracleObj.AddComponent<Oracle>();
+            managerObj = new GameObject();
+            manager = managerObj.AddComponent<QuestManager>();
+            quest = ScriptableObject.CreateInstance<QuestData>();
+            quest.questId = "Q1";
+            quest.npcId = "NPC1";
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(managerObj);
+            Object.DestroyImmediate(oracleObj);
+            Object.DestroyImmediate(quest);
+            Oracle.oracle = null;
+        }
+
+        [Test]
+        public void TryStartQuest_DoesNotActivateWithoutNpc()
+        {
+            var method = typeof(QuestManager).GetMethod("TryStartQuest", BindingFlags.NonPublic | BindingFlags.Instance);
+            method.Invoke(manager, new object[] { quest });
+            var field = typeof(QuestManager).GetField("active", BindingFlags.NonPublic | BindingFlags.Instance);
+            var dict = (System.Collections.IDictionary)field.GetValue(manager);
+            Assert.IsFalse(dict.Contains("Q1"));
+        }
+
+        [Test]
+        public void OnNpcMet_StartsQuest()
+        {
+            typeof(QuestManager).GetField("startingQuests", BindingFlags.NonPublic | BindingFlags.Instance)
+                .SetValue(manager, new List<QuestData> { quest });
+
+            StaticReferences.CompletedNpcTasks.Add("NPC1");
+            manager.OnNpcMet("NPC1");
+
+            var field = typeof(QuestManager).GetField("active", BindingFlags.NonPublic | BindingFlags.Instance);
+            var dict = (System.Collections.IDictionary)field.GetValue(manager);
+            Assert.IsTrue(dict.Contains("Q1"));
+        }
+    }
+}
+
