449da2b68b26bdf9c452b2c5cddbf17499701488
BlindsidedGames <blindsidedgames@outlook.com>
2025-07-07 19:53:48 +1000

    beginnings of regen

---
+++ Assets/Scripts/References/UI/RegenEntryUIReferences.cs
@@ -0,0 +1,16 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace References.UI
+{
+    public class RegenEntryUIReferences : MonoBehaviour
+    {
+        public CostResourceUIReferences costResourceUIReferences;
+        public TMP_Text fishNameText;
+        public TMP_Text amountDonatedText;
+        public TMP_Text regenText;
+        public Button donate10PercentButton;
+        public Button donateAllButton;
+    }
+}
