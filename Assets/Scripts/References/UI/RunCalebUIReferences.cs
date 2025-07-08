c7d9a27bab361485ea2c797daed21666fc1af573
BlindsidedGames <blindsidedgames@outlook.com>
2025-07-08 16:56:14 +1000

    runcalebreferences

---
+++ Assets/Scripts/References/UI/RunCalebUIReferences.cs
@@ -0,0 +1,15 @@
+using Blindsided.Utilities;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace References.UI
+{
+    public class RunCalebUIReferences : MonoBehaviour
+    {
+        public TMP_Text leftText;
+        public TMP_Text rightText;
+        public Button skillsButton;
+        public SlicedFilledImage healthBar;
+    }
+}
