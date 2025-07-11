03b2d2e6c35805b02dbbb146fae70ad629c56298
BlindsidedGames <blindsidedgames@outlook.com>
2025-07-11 13:27:08 +1000

    bvfg

--- Assets/Scripts/References/UI/BuffRecipeUIReferences.cs
+++ Assets/Scripts/References/UI/BuffRecipeUIReferences.cs
@@ -7,6 +7,8 @@ namespace References.UI
     public class BuffRecipeUIReferences : MonoBehaviour
     {
         public Image iconImage;
+        public TMP_Text nameText;
+        public TMP_Text descriptionText;
         public TMP_Text durationText;
         public Button purchaseButton;
         public CostResourceUIReferences costSlotPrefab;
