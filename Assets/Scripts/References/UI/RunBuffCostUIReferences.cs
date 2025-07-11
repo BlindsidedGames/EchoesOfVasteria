cbcd1a66394a1f5c8d8d1fc06dbba03b4cac0350
BlindsidedGames <blindsidedgames@outlook.com>
2025-07-11 12:47:23 +1000

    Tooltip stuff

---
+++ Assets/Scripts/References/UI/RunBuffCostUIReferences.cs
@@ -0,0 +1,13 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace References.UI
+{
+    public class RunBuffCostUIReferences : MonoBehaviour
+    {
+        public Image resourceIcon;
+        public TMP_Text resourceCostText;
+        public TMP_Text resourceHeldText;
+    }
+}
---
+++ Assets/Scripts/References/UI/RunBuffTooltipUIReferences.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace References.UI
+{
+    public class RunBuffTooltipUIReferences : MonoBehaviour
+    {
+        public GameObject tooltipPanel;
+        public Transform tooltipCostParent;
+        public RunBuffCostUIReferences tooltipCostPrefab;
+    }
+}
