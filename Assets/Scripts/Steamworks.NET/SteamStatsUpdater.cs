658932082dd14483214e2f0b241b77b89e38a578
BlindsidedGames <71115573+BlindsidedGames@users.noreply.github.com>
2025-07-14 19:24:08 +1000

    Add Steam stats updater

---
+++ Assets/Scripts/Steamworks.NET/SteamStatsUpdater.cs
@@ -0,0 +1,112 @@
+#if !(UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX || UNITY_STANDALONE_OSX || STEAMWORKS_WIN || STEAMWORKS_LIN_OSX)
+#define DISABLESTEAMWORKS
+#endif
+
+using UnityEngine;
+#if !DISABLESTEAMWORKS
+using Steamworks;
+#endif
+
+namespace TimelessEchoes
+{
+    /// <summary>
+    /// Updates Steam user stats based on values from GameplayStatTracker.
+    /// </summary>
+    public class SteamStatsUpdater : MonoBehaviour
+    {
+#if !DISABLESTEAMWORKS
+        private static SteamStatsUpdater instance;
+
+        /// <summary>
+        /// Singleton instance accessor.
+        /// </summary>
+        public static SteamStatsUpdater Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = FindFirstObjectByType<SteamStatsUpdater>();
+                    if (instance == null)
+                        instance = new GameObject("SteamStatsUpdater").AddComponent<SteamStatsUpdater>();
+                }
+
+                return instance;
+            }
+        }
+
+        [SerializeField]
+        private float updateInterval = 5f;
+        private float lastUpdate;
+
+        private void Awake()
+        {
+            if (instance != null && instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void Update()
+        {
+            if (Time.unscaledTime - lastUpdate >= updateInterval)
+            {
+                lastUpdate = Time.unscaledTime;
+                UpdateStats();
+            }
+        }
+
+        /// <summary>
+        /// Updates the DistanceReached, TotalKilometers and TasksCompleted stats.
+        /// </summary>
+        public void UpdateStats()
+        {
+            if (!SteamManager.Initialized)
+                return;
+
+            var tracker = Stats.GameplayStatTracker.Instance;
+            if (tracker == null)
+                return;
+
+            bool changed = false;
+
+            if (SteamUserStats.GetStat("DistanceReached", out int storedDistance))
+            {
+                int newDistance = Mathf.FloorToInt(tracker.LongestRun);
+                if (newDistance > storedDistance)
+                {
+                    SteamUserStats.SetStat("DistanceReached", newDistance);
+                    changed = true;
+                }
+            }
+
+            if (SteamUserStats.GetStat("TotalKilometers", out float storedKm))
+            {
+                float newKm = tracker.DistanceTravelled / 1000f;
+                if (newKm > storedKm)
+                {
+                    SteamUserStats.SetStat("TotalKilometers", newKm);
+                    changed = true;
+                }
+            }
+
+            if (SteamUserStats.GetStat("TasksCompleted", out int storedTasks))
+            {
+                int newTasks = tracker.TasksCompleted;
+                if (newTasks > storedTasks)
+                {
+                    SteamUserStats.SetStat("TasksCompleted", newTasks);
+                    changed = true;
+                }
+            }
+
+            if (changed)
+                SteamUserStats.StoreStats();
+        }
+#endif
+    }
+}
