1574619bdaf495e9d676efb4d6fb7562bef56631
BlindsidedGames <71115573+BlindsidedGames@users.noreply.github.com>
2025-07-19 07:53:15 +1000

    Add Disciple scriptable object and dynamic generation

--- Assets/Scripts/NpcGeneration/DiscipleGenerationManager.cs
+++ Assets/Scripts/NpcGeneration/DiscipleGenerationManager.cs
@@ -1,5 +1,10 @@
+using System.Collections;
 using System.Collections.Generic;
+using Blindsided.SaveData;
+using TimelessEchoes.Quests;
 using UnityEngine;
+using static Blindsided.EventHandler;
+using static Blindsided.Oracle;
 
 namespace TimelessEchoes.NpcGeneration
 {
@@ -10,19 +15,76 @@ namespace TimelessEchoes.NpcGeneration
     public class DiscipleGenerationManager : MonoBehaviour
     {
         public static DiscipleGenerationManager Instance { get; private set; }
-        [SerializeField] private List<DiscipleGenerator> generators = new();
+        [SerializeField] private List<Disciple> disciples = new();
+        [SerializeField] private DiscipleGenerator generatorPrefab;
+        [SerializeField] private Transform generatorParent;
+
+        private readonly List<DiscipleGenerator> generators = new();
 
         public IReadOnlyList<DiscipleGenerator> Generators => generators;
 
         private void Awake()
         {
             Instance = this;
+            OnLoadData += OnLoadDataHandler;
+            OnQuestHandin += OnQuestHandinHandler;
         }
 
         private void OnDestroy()
         {
+            OnLoadData -= OnLoadDataHandler;
+            OnQuestHandin -= OnQuestHandinHandler;
             if (Instance == this)
                 Instance = null;
+        }
+
+        private void OnLoadDataHandler()
+        {
+            StartCoroutine(DeferredBuild());
+        }
+
+        private void OnQuestHandinHandler(string questId)
+        {
+            StartCoroutine(DeferredBuild());
+        }
+
+        private IEnumerator DeferredBuild()
+        {
+            yield return null;
+            BuildGenerators();
+        }
+
+        private void BuildGenerators()
+        {
+            foreach (var gen in generators)
+                if (gen != null)
+                    Destroy(gen.gameObject);
+            generators.Clear();
+
+            if (generatorPrefab == null || generatorParent == null)
+                return;
+
+            foreach (var d in disciples)
+            {
+                if (d == null) continue;
+                if (!QuestCompleted(d.requiredQuest))
+                    continue;
+
+                var gen = Instantiate(generatorPrefab, generatorParent);
+                gen.name = d.name;
+                gen.SetData(d);
+                generators.Add(gen);
+            }
+        }
+
+        private static bool QuestCompleted(QuestData quest)
+        {
+            if (quest == null)
+                return true;
+            if (oracle == null)
+                return false;
+            oracle.saveData.Quests ??= new Dictionary<string, GameData.QuestRecord>();
+            return oracle.saveData.Quests.TryGetValue(quest.questId, out var rec) && rec.Completed;
         }
 
 
