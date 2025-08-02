f0665074461076e64f0a89cbfb37f39006304f68
BlindsidedGames <71115573+BlindsidedGames@users.noreply.github.com>
2025-08-02 19:34:33 +1000

    Revert "Use offsets instead of anchors for safe area"

--- Assets/Scripts/Blindsided/Utilities/ScreenSafeArea.cs
+++ Assets/Scripts/Blindsided/Utilities/ScreenSafeArea.cs
@@ -10,6 +10,8 @@ namespace Blindsided.Utilities
     public class ScreenSafeArea : MonoBehaviour
     {
         private RectTransform _rectTransform;
+        private Vector2 maxAnchor;
+        private Vector2 minAnchor;
         private Rect safeArea;
         private Vector2Int lastResolution;
         private Rect lastSafeArea;
@@ -183,8 +185,16 @@ namespace Blindsided.Utilities
                 }
             }
 
-            _rectTransform.offsetMin = new Vector2(safeArea.xMin, safeArea.yMin);
-            _rectTransform.offsetMax = new Vector2(-(Screen.width - safeArea.xMax), -(Screen.height - safeArea.yMax));
+            minAnchor = safeArea.position;
+            maxAnchor = minAnchor + safeArea.size;
+
+            minAnchor.x /= Screen.width;
+            minAnchor.y /= Screen.height;
+            maxAnchor.x /= Screen.width;
+            maxAnchor.y /= Screen.height;
+
+            _rectTransform.anchorMin = minAnchor;
+            _rectTransform.anchorMax = maxAnchor;
 
             lastSafeArea = Screen.safeArea;
             lastResolution = new Vector2Int(Screen.width, Screen.height);
